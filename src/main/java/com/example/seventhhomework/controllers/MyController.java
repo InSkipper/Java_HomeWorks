0be81f0cbd9b5dde8bfe7b86d4917f29b20b50c2
Daniel <den2002345@yandex.ru>
2022-05-12 20:56:19 +0500

    Update controller

--- src/main/java/com/example/seventhhomework/controllers/MyController.java
+++ src/main/java/com/example/seventhhomework/controllers/MyController.java
@@ -1,21 +1,15 @@
 package com.example.seventhhomework.controllers;
 
-import com.example.seventhhomework.dto.Input;
+import com.example.seventhhomework.dto.ToDoList;
 import com.example.seventhhomework.repositories.MyRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@Validated
-@RequiredArgsConstructor
 public class MyController {
-    private MyRepository repo;
+    private final MyRepository repo;
 
     @Autowired
     public MyController(MyRepository repo) {
@@ -23,14 +17,20 @@ public class MyController {
     }
 
     @PostMapping("/post")
-    public String readJSON(@RequestBody @Valid Input input) {
-        var entity = input.convertToEntity();
-`        repo.save(entity);
-        return String.format("Добавлен список дел №%d c именем '%s'", repo.count() + 1, input.getName());
+    public String readJSON(@RequestBody @Valid ToDoList input) {
+        repo.save(input);
+        return String.format("Добавлен список дел №%d c именем '%s'", input.getId(), input.getName());
     }
 
-//    @GetMapping("/get")
-//    public Iterable<Input> getDB() {
-//        return repo.findAll();
-//    }
+    @GetMapping("/get")
+    public Iterable<ToDoList> getDB() {
+        return repo.findAll();
+    }
+
+    @DeleteMapping("/clear")
+    public String clearDB() {
+        var count = repo.count();
+        repo.deleteAll();
+        return "Было удалено вхождений: " + count;
+    }
 }
