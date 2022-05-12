c91e72b90267c22ddc56eb7993aab37302105b97
Daniel <den2002345@yandex.ru>
2022-05-12 20:57:04 +0500

    Fix Validation

--- src/main/java/com/example/seventhhomework/dto/ToDoList.java
+++ src/main/java/com/example/seventhhomework/dto/ToDoList.java
@@ -2,8 +2,12 @@ package com.example.seventhhomework.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
@@ -12,13 +16,18 @@ import java.util.List;
 @Table(name = "todolists")
 public class ToDoList {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Long id;
 
-    @Column(name = "todolist_name", unique = true, nullable = false)
+    @Column(name = "name")
+    @Length(max = 50)
+    @NotBlank
     private String name;
 
     @ElementCollection
-    private List<String> events;
+    @Column(name = "events")
+    @Size(min = 1, max = 10)
+    @NotNull
+    private List<@NotBlank @Length(max = 50) String> events;
 }
