8c07ff69a96b1fa27ffa281e0957cdfb75c3aa32
Daniel <den2002345@yandex.ru>
2022-05-19 15:14:47 +0500

    Initial commit

--- src/main/java/com/example/defaultproject/DefaultProjectApplication.java
+++ src/main/java/com/example/defaultproject/DefaultProjectApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DefaultProjectApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DefaultProjectApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/example/defaultproject/aspect/WithApiMaxCountAspect.java
@@ -0,0 +1,36 @@
+package com.example.defaultproject.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class WithApiMaxCountAspect {
+    @Value("${api.max-count}")
+    private int maxCount;
+    private int currentCount;
+
+    @Pointcut("@annotation(com.example.defaultproject.annotation.WithApiMaxCount)")
+    public void withApiMaxCountMethods() {
+    }
+
+    @After("withApiMaxCountMethods()")
+    public void checkMaxCount(JoinPoint point) {
+        var methodName = String.format("%s.%s",
+                point.getStaticPart().getSignature().getDeclaringType().getName(),
+                point.getSignature().getName());
+        if (maxCount > currentCount) {
+            currentCount++;
+            log.info("Method {} was called {} times out of {}", methodName, currentCount, maxCount);
+        } else {
+            log.warn("Method {} has reached the max number of calls : {}", methodName, maxCount);
+            throw new RuntimeException("MAX number of calls");
+        }
+    }
+}
