ff849d1759fe3bd45e930072c7c3374092ccbf92
Daniel <den2002345@yandex.ru>
2022-05-21 21:50:35 +0500

    Fix Actuator

--- src/main/java/com/example/defaultproject/actuator/MyActuator.java
+++ src/main/java/com/example/defaultproject/actuator/MyActuator.java
@@ -1,6 +1,5 @@
 package com.example.defaultproject.actuator;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
@@ -10,11 +9,10 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 @Endpoint(id = "custom")
-@Slf4j
 public class MyActuator {
 
     @ReadOperation
-    public String getStatus() {
+    public String getTime() {
         var dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy  HH:mm:ss");
         var now = LocalDateTime.now();
         return "Текущая дата и время: " + now.format(dtf);
